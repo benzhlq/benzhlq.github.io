c03b5ac40c44a96a143fc059d424aedd8558cdc7
benzhlq <63956568+benzhlq@users.noreply.github.com>
2025-01-22 19:43:06 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/config', (req, res) => {
+  res.json({ webhookUrl: process.env.DISCORD_WEBHOOK_URL });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
