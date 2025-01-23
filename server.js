3f5cfef42545dc20d59bcb6127ef32746f54df95
benzhlq <63956568+benzhlq@users.noreply.github.com>
2025-01-23 18:33:11 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -14,8 +17,6 @@ app.get('/', (req, res) => {
 app.get('/config', (req, res) => {
   res.json({ webhookUrl: process.env.DISCORD_WEBHOOK_URL });
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
