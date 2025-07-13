ccbb9ab0a9deba63f9c8a414aacf3762ce54705d
benzhlq <63956568+benzhlq@users.noreply.github.com>
2025-07-13 13:07:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,23 @@ const port = 3000;
 
 app.use(express.static(__dirname));
 
+// Add CORS headers for the config endpoint
+app.use('/config', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/config', (req, res) => {
+  console.log('Webhook URL:', process.env.DISCORD_WEBHOOK_URL); // Debug log
   res.json({ webhookUrl: process.env.DISCORD_WEBHOOK_URL });
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+  console.log('Environment loaded:', !!process.env.DISCORD_WEBHOOK_URL);
 });
