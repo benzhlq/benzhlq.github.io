18a187728ad37af88e993d1859f425c668b8a600
benzhlq <63956568+benzhlq@users.noreply.github.com>
2025-10-10 23:47:47 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Public, non-secret OAuth constants only
+export const CLIENT_ID = "1328128686649643028";
+export const REDIRECT_URI = encodeURIComponent("https://honeycombbot.xyz");
+export const SCOPE = "identify";
